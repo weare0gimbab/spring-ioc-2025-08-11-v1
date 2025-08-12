6a877afec7db2381c6fce2af30eb5be5517016ae
yeonseo <weare0@yeonseoui-MacBookPro.local>
2025-08-12 17:36:31 +0900

    t2 testPostService 빈 얻기

--- src/main/java/com/ll/domain/testPost/testPost/repository/TestPostRepository.java
+++ src/main/java/com/ll/domain/testPost/testPost/repository/TestPostRepository.java
@@ -1,4 +1,7 @@
 package com.ll.domain.testPost.testPost.repository;
 
 public class TestPostRepository {
+    public TestPostRepository() {
+
+    }
 }
--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,20 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if ("testPostService".equals(beanName)) {
+            TestPostRepository testPostRepository = new TestPostRepository();
+
+            return (T) new TestPostService(testPostRepository);
+        }
+        return null;
     }
 }
+
