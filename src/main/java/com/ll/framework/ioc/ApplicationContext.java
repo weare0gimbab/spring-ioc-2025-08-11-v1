c456663d78a71d59ab67cecd8e179328c6f9c0ce
yeonseo <weare0@yeonseoui-MacBookPro.local>
2025-08-13 17:34:40 +0900

    t3 testPostService 빈을 다시 얻기, 싱글톤

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -3,18 +3,30 @@ package com.ll.framework.ioc;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
+    // bean저장할 해쉬맵
+    private static Map<String, Object> beans = new HashMap<>();
+
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
+        //beans에 존재하는 이름이면 그 빈 반환
+        if (beans.containsKey(beanName)) {
+            return (T) beans.get(beanName);
+        }
+
+        Object bean;
         if ("testPostService".equals(beanName)) {
             TestPostRepository testPostRepository = new TestPostRepository();
-
-            return (T) new TestPostService(testPostRepository);
-        }
-        return null;
+            bean = new TestPostService(testPostRepository);
+            beans.put(beanName, bean);
+        } else return null;
+        return (T) bean;
     }
 }
 
